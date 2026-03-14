2541597f99ce1521e227031b5cf161695e3e1a7e
Roshan Thapa Magar <roshan@octspace.com>
2026-03-14 09:30:07 +0545

    cross button remove in bag

--- components/header/Bag.tsx
+++ components/header/Bag.tsx
@@ -98,7 +98,7 @@ const Bag = () => {
         <SheetHeader className="border-b px-4 py-4 flex flex-row items-center justify-between">
           <SheetTitle className="text-lg sm:text-xl">Your Bag ({itemCount})</SheetTitle>
           <SheetClose className="rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-            <X className="h-4 w-4" />
+            {/* <X className="h-4 w-4" /> */}
             <span className="sr-only">Close</span>
           </SheetClose>
         </SheetHeader>
